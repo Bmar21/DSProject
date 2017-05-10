6c8a45d50bd9a356f3a22558c8ddcd8785a27ffe
Bmar21 <brandonmalor21@gmail.com>
2017-05-10 13:10:11 -0600

    added add and remove to the queue

--- src/ds/model/Customer.java
+++ src/ds/model/Customer.java
@@ -1,14 +1,6 @@
 package ds.model;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class Customer 
 {
-	private Queue<Customer> Customer;
 	
-	public Customer()
-	{
-		Customer = new LinkedList<Customer>();
-	}
 }
