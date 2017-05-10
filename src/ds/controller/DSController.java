ff955421383b9b26dcf122268a158e44182b54b8
Bmar21 <brandonmalor21@gmail.com>
2017-05-10 13:09:57 -0600

    added peek and push to the stack

--- src/ds/controller/DSController.java
+++ src/ds/controller/DSController.java
@@ -3,10 +3,24 @@ package ds.controller;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
-
+import ds.model.*;
 
 public class DSController 
 {
+	private Stack<Crackers> Crackers;
+	private Queue<Customer> Customer;
+	
+	public void Crackers()
+	{
+		
+	}
+	
+	public void Customer()
+	{
+		Customer = new LinkedList<Customer>();
+	}
+	
+	
 	public void start()
 	{
 		
